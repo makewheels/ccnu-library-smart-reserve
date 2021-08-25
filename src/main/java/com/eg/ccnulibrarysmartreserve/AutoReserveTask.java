cb94f8c35b9aca5143879e611335b5e7a598bbf4
miuser <miuser@email.com>
2021-08-25 18:54:40 +0800

    日志打印

--- src/main/java/com/eg/ccnulibrarysmartreserve/AutoReserveTask.java
+++ src/main/java/com/eg/ccnulibrarysmartreserve/AutoReserveTask.java
@@ -1,7 +1,9 @@
 package com.eg.ccnulibrarysmartreserve;
 
+import com.alibaba.fastjson.JSON;
 import com.eg.ccnulibrarysmartreserve.bean.User;
 import com.eg.ccnulibrarysmartreserve.bean.reserve.ReserveResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Slf4j
 public class AutoReserveTask {
     @Resource
     private ReserveService reserveService;
@@ -27,11 +30,11 @@ public class AutoReserveTask {
         me.setUsername("2020180007");
         me.setPassword("q63zuQushMESw3V");
         userList.add(me);
-        System.out.println("定时任务启动，用户列表：");
+        log.info("定时任务启动，用户列表：");
         for (User user : userList) {
-            System.out.print(user.getUsername() + " ");
+            log.info(user.getUsername() + " ");
         }
-        System.out.println();
+        log.info("\n");
         for (User user : userList) {
             new Thread(() -> handleEachUser(user)).start();
         }
@@ -39,21 +42,21 @@ public class AutoReserveTask {
 
     private void handleEachUser(User user) {
         String username = user.getUsername();
-        System.out.println("开始为用户预约: " + username);
+        log.info("开始为用户预约: " + username);
         HttpCookie cookie = reserveService.loginAndGetCookie(username, user.getPassword());
         user.setCookie(cookie);
         for (int i = 0; i < 20; i++) {
-            System.out.println("开始预约第 " + (i + 1) + " 次");
+            log.info("开始预约第 " + (i + 1) + " 次");
             LocalDateTime start = LocalDateTime.now().plusDays(1).withHour(10).withMinute(0);
             LocalDateTime end = LocalDateTime.now().plusDays(1).withHour(21).withMinute(0);
             ReserveResponse reserve = reserveService.reserve(user.getCookie(), "101700014",
                     start.toInstant(ZoneOffset.of("+8")).toEpochMilli(),
                     end.toInstant(ZoneOffset.of("+8")).toEpochMilli());
-            System.out.println(reserve);
+            log.info(JSON.toJSONString(reserve));
             int ret = reserve.getRet();
             //如果预约成功
             if (ret == 1) {
-                System.out.println("预约成功: " + user.getUsername());
+                log.info("预约成功: " + user.getUsername());
                 break;
             }
             try {
@@ -62,7 +65,7 @@ public class AutoReserveTask {
                 e.printStackTrace();
             }
         }
-        System.out.println("为指定用户预约超次数: " + user.getUsername());
+        log.info("为指定用户预约超次数: " + user.getUsername());
     }
 
 }
