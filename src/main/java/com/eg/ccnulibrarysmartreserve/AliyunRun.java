9d8da1e60a19a5bec2afc6c17e73a91eafaa6864
makewheels <makewheels>
2021-12-26 11:47:32 +0800

    commit

---
+++ src/main/java/com/eg/ccnulibrarysmartreserve/AliyunRun.java
@@ -0,0 +1,23 @@
+package com.eg.ccnulibrarysmartreserve;
+
+import com.aliyun.fc.runtime.Context;
+import com.aliyun.fc.runtime.StreamRequestHandler;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+
+public class AliyunRun implements StreamRequestHandler {
+    private final SyncHandler syncHandler = new SyncHandler();
+
+    @Override
+    public void handleRequest(InputStream input, OutputStream output, Context context) {
+        try {
+            syncHandler.run();
+        } catch (GitAPIException | URISyntaxException | IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
